4eaa80234ba1a58a56a06217027d5b02354482e0
Gerard <gerard@protofy.xyz>
2023-11-13 00:56:36 +0100

    adds basic components and examples to packages my/ui

--- packages/app/bundles/test/screen.tsx
+++ packages/app/bundles/test/screen.tsx
@@ -1,23 +1,34 @@
 import {
   H1,
   YStack,
+  Button,
+  Paragraph
 } from '@my/ui'
-import { Button } from 'tamagui';
 import React, { useEffect } from 'react'
 import { useTestStore } from './context/useTestStore';
+import { DefaultLayout, SectionTinted, ThemeTint, TintSection, getDefaultLayout } from '@my/ui/src';
 
 export function TestScreen(props) {
   console.log(props)
-  const {test, setTest} = useTestStore()
+  const { test, setTest } = useTestStore()
   useEffect(() => {
     console.log('TEST ATOM: ', test)
-  },[test])
+  }, [test])
   return (
-    <YStack f={1} jc="center" ai="center" p="$4" space>
-      <YStack space="$4" maw={600}>
-        <H1 ta="center">Test screen.</H1>
-        <Button onPress={() => setTest("Patata") }>Change test atom</Button>
-      </YStack>
-    </YStack>
+    <YStack>
+      <DefaultLayout
+        footer={<></>}
+      >
+        <ThemeTint >
+          <YStack f={1} jc="center" ai="center" p="$4" space>
+            <YStack space="$4" maw={600}>
+              <YStack pe="none" zi={0} fullscreen className="bg-dot-grid mask-gradient-down" />
+              <H1 ta="center">Test screen.</H1>
+              <Button onPress={() => setTest("Patata")}>Change test atom</Button>
+            </YStack>
+          </YStack>
+        </ThemeTint>
+      </DefaultLayout >
+    </YStack >
   )
 }
---
+++ packages/ui/src/components/PageGlow.tsx
@@ -0,0 +1,75 @@
+import { useTint } from './Tints/useTint'
+import { memo, useMemo, useState } from 'react'
+import { ThemeName, YStack, isClient, useDebounce } from 'tamagui'
+
+import { useTintSectionIndex } from './TintSection'
+
+export const PageGlow = memo(() => {
+  const { tints, tint, name, tintIndex } = useTint()
+  const isHeroBelowColor = tint === 'blue' || tint === 'green' || tint === 'purple'
+  const [index, setIndex] = useState(0)
+  const isAtTop = index <= 1
+  const isOnHeroBelow = isAtTop && isHeroBelowColor
+  const [scrollTop, setScrollTopRaw] = useState(0)
+  const setScrollTop = useDebounce(setScrollTopRaw, 200)
+  const xs = 400
+  const scale = isOnHeroBelow ? 0.5 : 1
+
+  if (isClient) {
+    useTintSectionIndex((index) => {
+      setIndex(index)
+      const sy = document.documentElement?.scrollTop ?? 0
+      setScrollTop(sy + 100)
+    })
+  }
+
+  const glows = useMemo(() => {
+    return (
+      <>
+        {tints.map((cur, i) => {
+          const isDouble = name === 'xmas' || name === 'easter'
+          const active = isDouble ? i == 0 || i == 1 : cur === tint
+          const isOpposite = isDouble && cur === 'green' && tint !== cur
+          return (
+            <YStack
+              key={`${cur}${i}`}
+              overflow="hidden"
+              h="100vh"
+              w={1000}
+              theme={cur as ThemeName}
+              o={active ? 0.3 : 0}
+              fullscreen
+              left={`calc(50vw - 500px)`}
+              x={isOnHeroBelow ? 0 : isDouble ? (isOpposite ? -500 : 500) : 0}
+              scale={scale}
+              className="hero-blur"
+            />
+          )
+        })}
+      </>
+    )
+  }, [scale, tint, tints])
+
+  return (
+    <YStack
+      pos="absolute"
+      t={0}
+      l={0}
+      contain="layout"
+      pe="none"
+      animation="quick"
+      key={0}
+      zi={-1}
+      x={0}
+      y={scrollTop}
+      {...(isOnHeroBelow && {
+        animation: 'quick',
+        x: tintIndex === 2 ? -xs : tintIndex === 4 ? xs : 0,
+        y: 300,
+      })}
+      // display={isResizing ? 'none' : 'flex'}
+    >
+      {glows}
+    </YStack>
+  )
+})
